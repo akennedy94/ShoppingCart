e1504d2da77fe9585d6b43c4adf6bd524f48fc66
akennedy94 <71675206+akennedy94@users.noreply.github.com>
2020-11-23 19:27:33 -0600

    Updated Cart.js html formating

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -75,7 +75,7 @@ const Cart = (props) => {
           <section>
             <div className="banner-innerpage">
               <div className="container">
-                <div className="row justify-content-center">
+                <div className="row mt-3 justify-content-center">
                   <div className="col-md-6 align-self-center text-center">
                     <h1 className="title">Cart Listing</h1>
                     <h6 className="subtitle op-8">Ready to checkout?</h6>
@@ -87,13 +87,14 @@ const Cart = (props) => {
           <section>
             <div className="container">
               <div className="row mt-5">
-                <div className="col-lg-9">
+                <div className="col-lg-10">
                   <div className="row shop-listing">
                     <table className="table shop-table">
                       <tr>
                         <th className="b-0">Name</th>
                         <th className="b-0">Price</th>
                         <th className="b-0">Quantity</th>
+                        <th className='spacer'></th>
                         <th className="b-0 text-right">Total Price</th>
                       </tr>
                       {cart.map(product => (
@@ -109,7 +110,7 @@ const Cart = (props) => {
                           </td>
                           <td>
                             <button onClick={() => removeItemFromCart(product.productId)}
-                            className="btn btn-primary">Remove from cart?</button>
+                            className="btn btn-danger">Remove from cart?</button>
                           </td>
                           <td className="text-right">
                             <h5 className="font-medium m-b-30">${product.productPrice * product.productAmount}</h5>
